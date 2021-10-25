d6589ae9b97f79691d9982579660ca55fd1f8fcc
jassoLuz <jassolucii@gmail.com>
2021-10-25 14:52:56 -0500

    dotenv install

--- routes/auth.js
+++ routes/auth.js
@@ -5,28 +5,10 @@ var path = require('path');
 var router = express.Router();
 
 var bd=require('./bd');
-//res.render('login',{login:results,page:"login"});
-//var app = express();
 
-//router.use(session({
-//	secret: 'secret',
-//	resave: true,
-//	saveUninitialized: true
-//}));
 router.use(bodyParser.urlencoded({extended : true}));
 router.use(bodyParser.json());
 
-//router.get('/', function(req, res, next) {
-  //  //res.render('login',{page:"alumnos",ope:"c"});
-    //res.render('login',{page:"login"});
-  //  res.sendFile(path.join(__dirname + '/views/login.hbs'));
-//});
-//display login page
-//display login page
-
-//app.get('/', function(request, response) {
-//	response.sendFile(path.join(__dirname + '/login.hbs'));
-//});
 router.get('/', function(req, res, next) {
   res.render('auth', { title: 'Login' });
 });
@@ -51,27 +33,5 @@ router.post('/authentication', function(request, response) {
 	}
 });
 
-//display home page
-//router.get('/', function(req, res, next) {
-  //  if (req.session.loggedin) {
-         
-    //    res.render('auth/index', {
-      //      title:"BazarWebApp",
-        //    name: req.session.name,     
-       // });
- 
-    //} else {
- 
-      //  req.flash('success', 'Please login first!');
-       // res.redirect('/auth');
-    //}
-//});
- 
-// Logout user
-//router.get('/logout', function (req, res) {
-  //req.session.destroy();
-  //req.flash('success', 'Login Again Here');
-  //res.redirect('/login');
-//});
 
 module.exports = router;
